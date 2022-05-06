06af79898d0707a6674b56f9a465b43ba560d549
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-05-06 14:59:01 +0200

    push

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-let hamburger = document.querySelector("div.hamburger-menu");
 
-    hamburger.addEventListener('click');
-    function myfunction() {
-    hamburger = document.createElement('ul');
-    hamburger = document.createElement('li');
-    li.append("home");
-    li.append("Scopri il corso");
-    li.append("la nostra storia");
-    li.append("Iscriviti")
-    }
+    const div = document.querySelector("div.header-right");
+    const ul = document.createElement("ul");
+    const li = document.createElement("li");
+    div.addEventListener('click',
+        function() {
+        li.append("home");
+        li.append("Scopri il corso");
+        li.append("la nostra storia");
+        li.append("Iscriviti");
+        ul.append(li);
+        div.append(ul);
+        }
+    );
