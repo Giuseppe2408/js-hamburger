07c5fd845f39411757fa19e23221f3a1852123ff
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-05-06 15:11:05 +0200

    correziono

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 
     const div = document.querySelector("div.header-right");
     let ul= document.querySelector('ul')
+    let li= document.querySelector('li')
     div.addEventListener('click',
         function() {
-            ul.style.display = "block";
-        
+            ul.style.display = "block"
+            li.style.float = "none"
+            li.style.display = "block"
         }
     );
