9001be0fd6db6bbcf0967edb611504358ac678e3
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-05-06 15:06:01 +0200

    modifiche

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 
     const div = document.querySelector("div.header-right");
-    const ul = document.createElement("ul");
-    const li = document.createElement("li");
+    let ul= document.querySelector('ul')
     div.addEventListener('click',
         function() {
-        li.append("home");
-        li.append("Scopri il corso");
-        li.append("la nostra storia");
-        li.append("Iscriviti");
-        ul.append(li);
-        div.append(ul);
+            ul.style.display = "block";
+        
         }
     );
